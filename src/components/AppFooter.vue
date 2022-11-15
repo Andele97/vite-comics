6e5d60e86314673e6d6c7e0a1388a6db3baa87e5
Andele97 <andreapinna40@live.it>
2022-11-15 19:56:29 +0100

    [add] footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -5,9 +5,120 @@ export default {
 </script>
 
 <template>
-  
+  <footer>
+
+    <div class="footer-up">
+      <div class="container">
+        <div class="cont-link">
+          <div class="link">
+            <nav>
+              <ul>
+                <h4>dc comics</h4>
+                <li><a href="#">Characters</a></li>
+                <li><a href="#">Comics</a></li>
+                <li><a href="#">Movies</a></li>
+                <li><a href="#">TV</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Videos</a></li>
+                <li><a href="#">News</a></li>
+              </ul>
+            </nav>
+          </div>
+          <div class="link">
+            <nav>
+              <ul>
+                <h4>shop</h4>
+                <li><a href="#">Shop DC</a></li>
+                <li><a href="#">Shop DC Collectibles</a></li>
+              </ul>
+            </nav>
+          </div>
+          <div class="link">
+            <nav>
+              <ul>
+                <h4>dc</h4>
+                <li><a href="#">Terms Of Use</a></li>
+                <li><a href="#">Privacy policy (New)</a></li>
+                <li><a href="#">Ad Choices</a></li>
+                <li><a href="#">Advertising</a></li>
+                <li><a href="#">Jobs</a></li>
+                <li><a href="#">Subscriptions</a></li>
+                <li><a href="#">Talent WorkShops</a></li>
+                <li><a href="#">CPSC Certificates</a></li>
+                <li><a href="#">Ratings</a></li>
+                <li><a href="#">Shop Help</a></li>
+                <li><a href="#">Contact Us</a></li>
+              </ul>
+            </nav>
+          </div>
+          <div class="link">
+            <nav>
+              <ul>
+                <h4>sites</h4>
+                <li><a href="#">DC</a></li>
+                <li><a href="#">MAD Magazine</a></li>
+                <li><a href="#">DC Kids</a></li>
+                <li><a href="#">DC Universe</a></li>
+                <li><a href="#">DC Power Visa</a></li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+        <div class="image"></div>
+      </div>
+    </div>
+
+    <div class="footer-bottom"></div>
+  </footer>
 </template>
 
-<style>
+<style lang="scss" scoped>
 
+@use '../style/general' as *;
+@use '../style/partials/mixin' as *;
+
+  .footer-up{
+    height: 370px;
+    background-image: url(../assets/img/footer-bg.jpg);
+    background-size: cover;
+    .container{
+      @include centerFlex('between');
+      .cont-link{
+        display: flex;
+        flex-wrap: wrap;
+        .link{
+          background-color: yellow;
+          width: 150px;
+          height: 90px;
+          h4{
+            color: white;
+            text-transform: uppercase;
+            margin-bottom: 10px;
+          }
+          ul{
+            list-style: none;
+            a{
+              text-decoration: none;
+              color: #777D73;
+              font-size: 0.8em;
+              &:hover{
+                text-decoration: underline;
+              }
+            }
+          }
+        }
+      }
+      .image{
+        height: 100%;
+        background-image: url(../assets/img/dc-logo-bg.png);
+        background-repeat: no-repeat;
+        background-position: right; 
+      }
+    }
+  }
+
+  .footer-bottom {
+    height: 110px;
+    background-color: #303030;
+  }
 </style>
